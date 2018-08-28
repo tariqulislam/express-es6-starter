9ed5c1bf0a4edeb2d6d3093424f133d57b8d382b
tariqul islam rony <tariqul.islam.rony@gmail.com>
2018-08-28 07:36:42 +0600

    add the s3 bucket upload code

--- app.js
+++ app.js
@@ -1,127 +1,62 @@
-import createError from 'http-errors'
 import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
+import http from 'http';
+import { normalizePort } from './core/config'
+
+import busboy from 'connect-busboy'
+import busboyBodyParser from 'busboy-body-parser'
+
 
 import indexRouter from './routes/index'
 import usersRouter from './routes/users'
 
 
 let app = express();
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
+app.use(busboy());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(busboyBodyParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-import http from 'http';
-
-/**
- * Get port from environment and store in Express.
- */
-
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 /**
  * Create HTTP server.
  */
-
 const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
+server.on('listening',  () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
   console.log('Listening on ' + bind);
-}
+});
 
-
-export default app;
+export default app
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,51 @@
 import  express from 'express'
-import path from 'path'
 const router = express.Router();
+import Busboy from 'busboy'
+import dotenv from 'dotenv'
+const env = dotenv.config()
 
+const BUCKET_NAME = 'bucketnewnamefortest'
+const IAM_USER_KEY= ''
+const IAM_USER_SECRET = ''
 
-var expressPDF = require('express-pdf-report');
-var columns = {id:"id", first_name: "First name", last_name: "Last name", email: "Email", gender: "Gender", ip_address: "IP Address"};
-var data=[
-{"id":1,"first_name":"Sydelle","last_name":"Trotman","email":"strotman0@house.gov","gender":"Female","ip_address":"200.242.49.228"},
-{"id":2,"first_name":"Tod","last_name":"Wisniowski","email":"twisniowski1@nih.gov","gender":"Male","ip_address":"88.217.79.159"},
-{"id":3,"first_name":"Falito","last_name":"Hannah","email":"fhannah2@google.pl","gender":"Male","ip_address":"138.46.94.36"},
-{"id":4,"first_name":"Luce","last_name":"Terlinden","email":"lterlinden3@ycombinator.com","gender":"Female","ip_address":"41.14.169.220"},
-{"id":5,"first_name":"Nona","last_name":"Sedworth","email":"nsedworth4@issuu.com","gender":"Female","ip_address":"115.46.27.214"},
-{"id":6,"first_name":"Eamon","last_name":"Cremins","email":"ecremins5@nature.com","gender":"Male","ip_address":"159.10.192.245"},
-{"id":7,"first_name":"Augie","last_name":"Meanwell","email":"ameanwell6@wikia.com","gender":"Male","ip_address":"137.254.6.40"},
-{"id":8,"first_name":"Payton","last_name":"Maxsted","email":"pmaxsted7@storify.com","gender":"Male","ip_address":"151.161.157.46"},
-{"id":9,"first_name":"Dorelia","last_name":"Brise","email":"dbrise8@slideshare.net","gender":"Female","ip_address":"243.34.167.90"},
-{"id":10,"first_name":"Clement","last_name":"Prisk","email":"cprisk9@furl.net","gender":"Male","ip_address":"157.163.221.36"},
-{"id":11,"first_name":"Rossy","last_name":"Chazotte","email":"rchazottea@mac.com","gender":"Male","ip_address":"154.244.122.148"},
-{"id":12,"first_name":"Porter","last_name":"Leidecker","email":"pleideckerb@google.co.jp","gender":"Male","ip_address":"145.226.221.115"},
-{"id":13,"first_name":"Stacy","last_name":"Boays","email":"sboaysc@vistaprint.com","gender":"Female","ip_address":"10.213.11.117"},
-{"id":14,"first_name":"Lindsay","last_name":"Guerin","email":"lguerind@slideshare.net","gender":"Male","ip_address":"185.114.149.58"}
-];
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+function uploadToS3 (file) {
+  let s3bucket = new AWS.S3({
+    accessKeyId: IAM_USER_KEY,
+    secretAccessKey: IAM_USER_SECRET,
+    Bucket: BUCKET_NAME
+  });
 
-router.get('/generate/pdf/force', (req, res, next) => {
-  var filename = 'employee' + Math.floor((Math.random()* 50) + 1)+ '.pdf';
-  expressPDF.renderForceDownloadPDF(res, columns,data, filename);
-});
+  s3bucket.createBucket(() => {
+    var param = {
+      Bucket: BUCKET_NAME,
+      Key: file.name,
+      Body: file.data
+    };
 
-router.get('/generate/pdf', function(req, res, next) {
-  var location = path.join(__dirname, 'employee.pdf');
-  console.log(location);
- expressPDF.renderListPDFToDir(columns, data, location);
+    s3bucket.upload(param, (err, data) => {
+      if(err) {
+        console.log('error in call back');
+        console.log(err);
+      }
 
- res.status(200).send({output: 'success', message: 'pdf create successfully'})
- res.end();
+      console.log('success');
+      console.log(data);
+    })
+  })
+}
 
+router.post('/api/upload/s3', (req, res, next) => {
+  const busboy = new Busboy({ headers: req.headers})
 
+  busboy.on('finish', () => {
+    console.log('upload finish')
+    const file = req.files.exampleImage
+    console.log(file)
+    uploadToS3(file)
+  })
+
+  req.pipe(busboy)
 })
+
 
 export default router
